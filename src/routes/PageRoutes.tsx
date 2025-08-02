34552a2dc0cb7751a5389bde952070802640e9ed
shafiqzy <shafiqzy234@gmail.com>
2025-08-03 00:36:52 +0800

    fix: update page route

--- src/routes/PageRoutes.tsx
+++ src/routes/PageRoutes.tsx
@@ -1,12 +1,23 @@
 import { Route, Switch } from "wouter";
 import { ButtonPage } from "./LazyPages";
+import { ROUTES } from "src/constants/routes";
+import BaseLayout from "src/components/Layout/BaseLayout";
+import DashboardPage from "src/pages/Dashboard/DashboardPage";
 
 export default function PageRoutes() {
   return (
-    <Switch>
-      <Route path="/">
-        <ButtonPage />
-      </Route>
-    </Switch>
+    <BaseLayout>
+      <Switch>
+        <Route path={ROUTES.home}>
+          <ButtonPage />
+        </Route>
+        <Route path={ROUTES.dashboard}>
+          <DashboardPage />
+        </Route>
+        <Route path={ROUTES.button}>
+          <ButtonPage />
+        </Route>
+      </Switch>
+    </BaseLayout>
   );
 }
